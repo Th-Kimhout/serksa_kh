75abb8afb0fed286c1e51b5d816d92dc6c8d66d4
Kong_Sisovandara <sisovandara@gmail.com>
2024-09-26 15:24:50 +0700

    updated

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,5 +1,5 @@
-import { CategoryCard } from "../../components/Cards/CategoryCard";
 import { PopularCard } from "../../components/Cards/PopularCard";
+import { CategoryCard } from "../../components/Cards/CategoryCard";
 import React from "react";
 import hero from "../../assets/Image/hero.png";
 
